8f5a7a1b22adcae1ac63b56cc892e8b2e02af51b
Lucas Santos <lucsantosdev@gmail.com>
2025-02-24 19:50:15 -0300

    mclaren navigation menu configurations

--- brands-car-list/mclaren/script.js
+++ brands-car-list/mclaren/script.js
@@ -1,5 +1,8 @@
 // toggle menu button
 function toggleMenu() {
     const menu = document.querySelector('.menu')
+    const nav = document.querySelector('.nav')
     menu.classList.toggle('active')
+    nav.classList.toggle('active')
+
 }
