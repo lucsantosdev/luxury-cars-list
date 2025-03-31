be54279db6bacac1b603dbd3adc70f3ba92f0b1a
Lucas Santos <lucsantosdev@gmail.com>
2025-03-31 19:38:28 -0300

    mclaren change bg and trailer videos using js

--- brands-car-list/mclaren/script.js
+++ brands-car-list/mclaren/script.js
@@ -4,5 +4,36 @@ function toggleMenu() {
     const nav = document.querySelector('.nav')
     menu.classList.toggle('active')
     nav.classList.toggle('active')
+}
 
+// change the bg video when click on the gallery images
+function changeVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video')
+    const trailers = document.querySelectorAll('.trailer')
+    const models = document.querySelectorAll('.model')
+
+    // js higher order array function forEach
+    // this is easier to do data mapping
+    bgVideoList.forEach(video => {
+        video.classList.remove('active')
+        if (video.classList.contains(name)) {
+            video.classList.add('active')
+        }
+    })
+
+    // mapping model name change
+    models.forEach(model => {
+        model.classList.remove('active')
+        if (model.classList.contains(name)) {
+            model.classList.add('active')
+        }
+    })
+
+    // mapping trailer video change
+    trailers.forEach(video => {
+        video.classList.remove('active')
+        if (video.classList.contains(name)) {
+            video.classList.add('active')
+        }
+    })
 }
